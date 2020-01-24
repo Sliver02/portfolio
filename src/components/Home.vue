2c34b27bbce88791802eae852f5a753dc82f1811
JPanzera <J.Panzera@eu.aswatson.com>
2020-01-24 18:02:39 +0100

    fix flex home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,30 +1,30 @@
 <template>
 
     <div id="main" :style="scssVars">
-        <div class="c-navbar">
-            <div class="c-navbar__logo">
+        <div class="navbar">
+            <div class="navbar__logo">
                 <h1><a href=".">jac.pan</a></h1>
             </div>
-            <div class="c-navbar__menu">
-                <ul class="l-nav">
+            <div class="navbar__menu">
+                <ul class="nav">
                     <!-- <li v-for="(page, index) in pages" :key="index">
-                        <a :href="'#'+page" class="l-nav__btn" @click="active = page" :class="{'is-active': active === page }">.{{page}}</a>
+                        <a :href="'#'+page" class="nav__btn" @click="active = page" :class="{'is-active': active === page }">.{{page}}</a>
                     </li> -->
 
                     <li v-for="(page, index) in pages" :key="index">
-                        <div class="l-nav__btn" @click="pageIndex = index" :class="{'is-active': pageIndex === index }">.{{page}}</div>
+                        <div class="nav__btn" @click="pageIndex = index" :class="{'is-active': pageIndex === index }">.{{page}}</div>
                     </li>
                 </ul>
             </div>
         </div>
         
         <!-- <div class="l-side" :style="{transform:' translate3d(calc(-100vw*' + pageIndex +'), 0, 0)'}"> -->
-        <div class="l-side">
-            <div class="l-section">
+        <div class="home">
+            <div class="home__section">
 
-                <div class="c-home">
-                    <div class="c-home__img"></div>
-                    <div class="c-home__desc">
+                <div class="preview">
+                    <div class="preview__img"></div>
+                    <div class="preview__desc">
                         <h1>20 Coperti</h1>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur explicabo illo alias repellat quam quas accusantium mollitia sed molestias! Aliquid itaque aspernatur dolore totam molestias cumque! Esse culpa dolores quidem, ex quod, incidunt labore quibusdam recusandae, iste ipsum dolorum. Velit?</p>
                     </div>
@@ -32,11 +32,31 @@
 
             </div>
 
-            <div class="l-section">
+            <div class="home__section">
 
-                <div class="c-home">
-                    <div class="c-home__img"></div>
-                    <div class="c-home__desc">
+                <div class="about">
+                    <div class="about__desc">
+                        <h1>jacopo<br> .panzera</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, voluptate beatae? Magni amet, excepturi quasi consequuntur nulla quam placeat saepe. Ut eos illum repellat ea sit. Alias pariatur quibusdam eveniet officiis soluta numquam, nihil accusamus, dolore necessitatibus quis provident perspiciatis.</p>
+                    </div>
+                    <div class="about__img"></div>
+                </div>
+
+                <!-- <div class="preview">
+                    <div class="preview__img"></div>
+                    <div class="preview__desc">
+                        <h1>20 Coperti</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur explicabo illo alias repellat quam quas accusantium mollitia sed molestias! Aliquid itaque aspernatur dolore totam molestias cumque! Esse culpa dolores quidem, ex quod, incidunt labore quibusdam recusandae, iste ipsum dolorum. Velit?</p>
+                    </div>
+                </div> -->
+
+            </div>
+
+            <div class="home__section">
+
+                <div class="preview">
+                    <div class="preview__img"></div>
+                    <div class="preview__desc">
                         <h1>20 Coperti</h1>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur explicabo illo alias repellat quam quas accusantium mollitia sed molestias! Aliquid itaque aspernatur dolore totam molestias cumque! Esse culpa dolores quidem, ex quod, incidunt labore quibusdam recusandae, iste ipsum dolorum. Velit?</p>
                     </div>
@@ -44,23 +64,11 @@
 
             </div>
 
-            <div class="l-section">
+            <div class="home__section">
 
-                <div class="c-home">
-                    <div class="c-home__img"></div>
-                    <div class="c-home__desc">
-                        <h1>20 Coperti</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur explicabo illo alias repellat quam quas accusantium mollitia sed molestias! Aliquid itaque aspernatur dolore totam molestias cumque! Esse culpa dolores quidem, ex quod, incidunt labore quibusdam recusandae, iste ipsum dolorum. Velit?</p>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="l-section">
-
-                <div class="c-home">
-                    <div class="c-home__img"></div>
-                    <div class="c-home__desc">
+                <div class="preview">
+                    <div class="preview__img"></div>
+                    <div class="preview__desc">
                         <h1>20 Coperti</h1>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur explicabo illo alias repellat quam quas accusantium mollitia sed molestias! Aliquid itaque aspernatur dolore totam molestias cumque! Esse culpa dolores quidem, ex quod, incidunt labore quibusdam recusandae, iste ipsum dolorum. Velit?</p>
                     </div>
---
+++ src/components/_About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="c-about">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+    
+</style>
