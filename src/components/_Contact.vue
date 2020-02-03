f4083954d74c06887f049f8f13e8a89757d8efc1
JPanzera <J.Panzera@eu.aswatson.com>
2020-02-03 11:27:07 +0100

    added form submission and captcha

--- src/components/_Contact.vue
+++ src/components/_Contact.vue
@@ -5,30 +5,34 @@
             <p>Let's get together to create something unique and charming</p>
         </div>
 
-        <form class="contact__form" action="">
+        <form class="contact__form" name="contact" action="POST" data-netlify="true">
             <div class="contact__name">
                 <label for="name">.name</label>
-                <input id="name" type="text">
+                <input name="name" id="name" type="text">
             </div>
 
             <div class="contact__title">
                 <label for="title">.title</label>
-                <input id="role" type="text">
+                <input name="role" id="role" type="text">
             </div>
 
             <div class="contact__email">
                 <label for="email">.e-mail</label>
-                <input id="email" type="text">
+                <input name="email" id="email" type="text">
             </div>
 
             <div class="contact__phone">
                 <label for="phone">.phone</label>
-                <input id="phone" type="text">
+                <input name="phone" id="phone" type="text">
             </div>
 
             <div class="contact__why">
                 <label for="why">.what can I do for you</label>
-                <textarea id="why" type="text"></textarea>
+                <textarea name="why" id="why" type="text"></textarea>
+            </div>
+
+            <div class="captcha">
+                <div data-netlify-recaptcha='true'></div>
             </div>
 
             <input class="btn" type="submit" value=".send">
