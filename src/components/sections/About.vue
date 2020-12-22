91e5b7d9777e42c3939bd91dbb4a0ba0994d95ab
JPanzera <J.Panzera@eu.aswatson.com>
2020-12-22 18:28:28 +0100

    mobile-first sections

--- src/components/sections/About.vue
+++ src/components/sections/About.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="about">
             
-            <img class="about__bg" :src="require('../../assets/img/icons/logo.svg')" alt="">
+        <img class="about__bg" :src="require('../../assets/img/icons/logo.svg')" alt="">
 
-            <div class="about__desc">
-                <h1><span class="jojo">j</span>ac<span class="jojo">o</span>po<br> .panzera</h1>
-                <p>Illustrator, graphic & web designer based in Milan.
-                I'm driven by curiosity and innovation, always striving to better myself and to find joy into what I do.
-                Working on unique and full of meaning projects gives me a strong porpouse and ethics.</p>
-            </div>
-            <div class="about__img"></div>
-            <div class="about__social">
-                <a href="https://www.linkedin.com/in/jacopo-panzera-b3a3b1167/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-                <a href="https://www.behance.net/panzerajac5508" target="_blank"><i class="fab fa-behance"></i></a>
-                <a href="https://www.instagram.com/jac.pan/" target="_blank"><i class="fab fa-instagram"></i></a>
-            </div>
+        <div class="about__img"></div>
+        <div class="about__desc">
+            <h1><span class="jojo">j</span>ac<span class="jojo">o</span>po<br> .panzera</h1>
+            <p>Illustrator, graphic & web designer based in Milan.
+            I'm driven by curiosity and innovation, always striving to better myself and to find joy into what I do.
+            Working on unique and full of meaning projects gives me a strong porpouse and ethics.</p>
         </div>
+        <div class="about__social">
+            <a href="https://www.linkedin.com/in/jacopo-panzera-b3a3b1167/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://www.behance.net/panzerajac5508" target="_blank"><i class="fab fa-behance"></i></a>
+            <a href="https://www.instagram.com/jac.pan/" target="_blank"><i class="fab fa-instagram"></i></a>
+        </div>
+    </div>
         
 
 </template>
